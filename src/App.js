96171bc35898d362125082d1634eb17a8cb866c7
jscodelover <manisha.4918@gmail.com>
2021-04-24 10:56:39 +0530

    feat: create UI for back and help link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from './components/Header';
+import Spinner from './components/Spinner';
+import ArrowLeft from './assets/images/arrow-left.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<main>
+			<Header />
+			<section className="game-wrapper">
+				<button className="show-rewards-btn">
+					<img src={ArrowLeft} alt="arrow-left.svg" />
+					Your rewards
+				</button>
+				<Spinner />
+				<div className="info">
+					<p>Spin the wheel now to get rewarded</p>
+					<p>
+						Tap on Spin or rotate the wheel anti-clockwise and release to start
+						spinning
+					</p>
+				</div>
+				<a href="#" className="help_btn">
+					Have a question? <span>Get help</span>
+				</a>
+			</section>
+		</main>
+	);
 }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import './style.scss';
+
+function Header() {
+	return (
+		<section className="header-wrapper">
+			<span />
+			<span />
+			<span />
+		</section>
+	);
+}
+
+export default Header;
